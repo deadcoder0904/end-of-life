4cd169c810a3c8209c66afe0080f6d549fdd816a
Akshay Kadam <akshaykadam0904@gmail.com>
2018-07-06 15:04:17 +0530

    Disappointment 😢

--- src/main/index.js
+++ src/main/index.js
@@ -1,16 +1,36 @@
-import { app, BrowserWindow, Tray, nativeImage } from "electron";
-import * as path from "path";
-import { format as formatUrl } from "url";
-import { sync as DataURI } from "datauri";
+import { app, BrowserWindow, Tray, nativeImage } from 'electron';
+import * as path from 'path';
+import { format as formatUrl } from 'url';
+import { sync as DataURI } from 'datauri';
+import { JSDOM } from 'jsdom';
+import getContext from 'get-canvas-context';
+// import svg2png from 'svg-to-png';
 
 // import icon from "../assets/progress-bar.svg";
 
-const isDevelopment = process.env.NODE_ENV !== "production";
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+// const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+// console.log(dom.window.document.querySelector('p').textContent);
+const dom = new JSDOM("<canvas width='99' height='101'></canvas>");
+const canvas = dom.window.document.querySelector('canvas');
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
 let mainWindow;
 
-function createMainWindow() {
+const drawInlineSVG = (svgElement, ctx, callback) => {
+  const svgURL = new JSDOM(
+    `new XMLSerializer().serializeToString(${svgElement})`
+  );
+  const img = new JSDOM(`new Image()`);
+  img.onload = function() {
+    ctx.drawImage(this, 0, 0);
+    callback();
+  };
+  img.src = `data:image/svg+xml; charset=utf8, ${encodeURIComponent(svgURL)}`;
+};
+
+const createMainWindow = () => {
   const window = new BrowserWindow();
 
   if (isDevelopment) {
@@ -22,23 +42,42 @@ function createMainWindow() {
   } else {
     window.loadURL(
       formatUrl({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file",
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
         slashes: true
       })
     );
   }
 
-  let trayIcon = nativeImage.createFromDataURL(
-    DataURI(path.join(__dirname, "../assets/progress-bar.svg"))
-  );
-  new Tray(trayIcon);
+  // await svg2png.convert(
+  //   path.join(__dirname, "../assets/progress-bar.svg"),
+  //   path.join(__dirname, "../assets/tmp/progress-bar.png")
+  // );
+  const ctxt = getContext('2d');
 
-  window.on("closed", () => {
+  drawInlineSVG(
+    path.join(__dirname, '../assets/progress-bar.svg'),
+    ctxt,
+    function() {
+      console.log(`===================`);
+      console.log(canvas.toDataURL());
+      console.log(`===================`);
+
+      let trayIcon = nativeImage.createFromDataURL(canvas.toDataURL());
+      new Tray(trayIcon);
+    }
+  );
+
+  // let trayIcon = nativeImage.createFromDataURL(
+  //   DataURI(path.join(__dirname, '../assets/progress-bar.png'))
+  // );
+  // new Tray(trayIcon);
+
+  window.on('closed', () => {
     mainWindow = null;
   });
 
-  window.webContents.on("devtools-opened", () => {
+  window.webContents.on('devtools-opened', () => {
     window.focus();
     setImmediate(() => {
       window.focus();
@@ -46,17 +85,17 @@ function createMainWindow() {
   });
 
   return window;
-}
+};
 
 // quit application when all windows are closed
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   // on macOS it is common for applications to stay open until the user explicitly quits
-  if (process.platform !== "darwin") {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   // on macOS it is common to re-create a window even after all windows have been closed
   if (mainWindow === null) {
     mainWindow = createMainWindow();
@@ -64,6 +103,6 @@ app.on("activate", () => {
 });
 
 // create main BrowserWindow when electron is ready
-app.on("ready", () => {
+app.on('ready', () => {
   mainWindow = createMainWindow();
 });
